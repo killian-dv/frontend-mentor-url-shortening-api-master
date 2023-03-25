2e476d1cb6ade3df30df23a5e8a4283cd57fe490
Killian David <killian.david@etudiant.univ-reims.fr>
2023-03-25 13:57:13 +0100

    menu hamburger

---
+++ script.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('.navigation');
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', openMenu);
+
+function openMenu() {
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+}
