9288bb96072e6a22eb89fa10b04f4ca49760ebaf
Killian David <killian.david@etudiant.univ-reims.fr>
2023-04-14 12:06:53 +0200

    ajout erreur formulaire

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Menu burger
-
 const menu = document.querySelector('.navigation');
 const hamburger = document.querySelector('.hamburger');
 
@@ -9,3 +8,21 @@ function openMenu() {
   hamburger.classList.toggle('active');
   menu.classList.toggle('active');
 }
+
+// Error message
+const input = document.querySelector('.input');
+const errorMessage = document.querySelector('.error-message');
+const button = document.querySelector('.send');
+
+button.addEventListener('click', () => {
+  if (input.value.trim() === '') {
+    input.style.border = 'hsl(0, 87%, 67%) solid 2px';
+    input.classList.add('error');
+    errorMessage.style.display = 'block';
+    console.log('error');
+  } else {
+    input.style.border = '';
+    errorMessage.style.display = '';
+    input.classList.remove('error');
+  }
+});
